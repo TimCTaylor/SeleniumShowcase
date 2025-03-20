0cb12cc4919637e9f8b23b5bc998c36ac5cafc0d
Tim Taylor <editors@greyhartpress.com>
2025-03-20 10:12:54 +0000

    fixing some java versioning issues

--- src/test/java/junit/tests/Sandbox2.java
+++ src/test/java/junit/tests/Sandbox2.java
@@ -5,19 +5,20 @@ public class Sandbox2 {
 
         // Change these boolean values to control whether you see 
         // the expected result and/or hints.
-        public static boolean showExpectedResult = false;
-        public static boolean showHints = true;
-    
+        public boolean showExpectedResult = false;
+        public boolean showHints = true;
+
         // Return the total character after adding quest experience as a JSON string.
         // Return null if there is a problem with the JSON
-        public static String calculateFinalExperience(String characterString, String questString) {
+        public String calculateFinalExperience(String characterString, String questString) {
             try {
                 return characterString;
-            } catch (Exception e)
-            { return null;}
-            
+            } catch (Exception e) {
+                return null;
+            }
+
         }
-    
+
     }
 
 }
