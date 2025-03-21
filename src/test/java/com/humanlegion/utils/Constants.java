a7940cfa3fd89f708066090ba42e86c152c7e919
Tim Taylor <editors@greyhartpress.com>
2025-03-21 12:04:29 +0000

    Reorganised to Maven folder structure

---
+++ src/test/java/com/humanlegion/utils/Constants.java
@@ -0,0 +1,19 @@
+package com.humanlegion.utils;
+
+public class Constants {
+    // I'm testing with this set to false, then turning off calls to open up my books on Amazon to reduce chance of Amazon banning me for unusual activity.
+    public static final boolean AVOID_OPENING_AMAZON_LINKS = true;
+
+    // Constants for the browser window size for when I want to have the browser
+    // window and the IDE placed one above the other when debugging.
+    public static final int BROWSER_DEBUG_HEIGHT = 550;
+    public static final int BROWSER_DEBUG_WIDTH = 1625;
+
+    public static final String COPYRIGHT_STATEMENT = "(c) Human Legion Publications 2025. All rights reserved.";
+    public static final String SCREENSHOT_FOLDER = ".\\screenshots\\";
+
+    public static final String LEGION_BASE_URL = "https://humanlegion.com/";
+    public static final String HORSEMEN_SERIES_URL = "https://humanlegion.com/books/4hu-four-horsemen-books/";
+    public static final String LEGION_SERIES_URL = "https://humanlegion.com/books/human-legion-books/";
+    public static final String TIMEDOGZ_SERIES_URL = "https://humanlegion.com/books/timedogz/";
+}
