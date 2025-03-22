701926175626e76ec275fd250c6eb8131ea26a2d
Tim Taylor <editors@greyhartpress.com>
2025-03-22 10:36:59 +0000

    wip

---
+++ src/main/java/com/humanlegion/bob/Bob.java
@@ -0,0 +1,18 @@
+package com.humanlegion.bob;
+import java.String;
+
+public class Bob {
+    private static final String LOFTY_MESSAGE = "Err, yes, I think so, Bob.";
+
+    public Bob() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println(LOFTY_MESSAGE);
+    }
+
+    public String canWeBuildIt() {
+        return LOFTY_MESSAGE;
+    }
+
+}
